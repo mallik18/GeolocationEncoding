871c6bbc4dea16be3ba31f71802039aad7a22d55
mallik18 <mallikarjunkolar18@gmail.com>
2022-04-21 17:45:12 +0530

    made some changes to running of the application

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # GeolocationEncoding
 Converts given Address to Geo Coordinates of Latitude and Longitude
 
+
+[ Running the Application on Linux O.S: ]
+
+1. Clone the application on your device.
+
+2. Create a python environment ( Python 3.8.10 ): python3 -m pip venv venv
+
+3. Activate the virtual environment: source venv/bin/activate 
+
+4. Install all the packages required for the application: pip install -r requirements.txt
+
+5. Give permission to run.sh file: chmod +x run.sh
+
+6. To start the Application execute run.sh file: ./run.sh
+
+[ Running the Application using Docker: ]
+
+1. Clone the application on your device.
+
+2. Give permission to run.sh file: chmod +x dockerrun.sh
+ 
+3. To start the Application execute dockerrun,sh file: ./dockerrun.sh
+
--- geolocation.py
+++ geolocation.py
@@ -2,6 +2,7 @@
 import os
 import requests
 import uvicorn
+import logging
 
 from fastapi import FastAPI
 from fastapi import Request
@@ -13,6 +14,8 @@ API_KEY = os.environ.get('GEO_API_KEY')
 
 app = FastAPI()
 
+logging.basicConfig(filename ='app.log',
+                        level = logging.DEBUG)
 
 @app.get("/", response_class=RedirectResponse, status_code=307)
 async def home():
@@ -48,19 +51,21 @@ async def convertaddresstocoordinates(request: Request):
     """
     try:
         payload = await request.json()
+
+        if payload['address'] == "" or payload['output_format'] == "":
+            return Response("Missing Parameter Values", status_code=400)
+
         # base_url
         base_url = "https://maps.googleapis.com/maps/api/geocode/json?"
 
         # sanitizing params for url encoding
-        address_data = payload['address'].strip('#').replace(
-            ' ', '+')
+        address_data = payload['address'].strip('#').replace(' ', '+')
 
         # url encoding
-        request_url = base_url + 'address=' + \
-            address_data + '&key=' + API_KEY
-
+        request_url = base_url + 'address=' + address_data + '&key=' + API_KEY
+        
         # requesting the result
-        res = requests.get(request_url).json()
+        res = requests.get(str(request_url)).json()
 
         # result in json format
         if payload['output_format'] == 'json':
@@ -87,17 +92,10 @@ async def convertaddresstocoordinates(request: Request):
             content=data,
             media_type="application/xml",
             status_code=200)
-
-    except requests.HTTPError as err:
-        return Response(err, status_code=400)
-    except TimeoutError as err:
-        return Response(err, status_code=408)
-    except ValidationError as err:
-        return Response(err, status_code=404)
-
-# Need to Do:
-# check for blank body and for each blank attribute
-# try with other ouptut formats
+    
+    except Exception as err:
+        logging.error(err)
+        return Response(err)
 
 if __name__ == "__main__":
-    uvicorn.run("geolocation:app", port=5000)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
